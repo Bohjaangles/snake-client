9f631c3c1c74f50a2cdb5bb0226504bf0d450882
Russel Mueller <bohjaangles@gmail.com>
2022-10-26 22:11:02 +0000

    added ability to display our name on the game server from the client.js file

--- client.js
+++ client.js
@@ -1,18 +1,25 @@
+const { write } = require('fs');
 const net = require('net');
 
 const connect = () => {
   const conn = net.createConnection({
     host: '165.227.47.243',
     port: 50541
-  })
+  });
   conn.setEncoding('utf8');
+  conn.on('connect', () => {
+    console.log('connection established');
+  });
+  conn.on('connect', () => {
+    conn.write('Name: rjm');
+  })
   conn.on('data', (data) => {
     console.log('server says:', data);
-  })
-  return conn
+  });
+  return conn;
 };
 
 console.log('connecting...');
 connect();
 
-module.exports = {connect};
+module.exports = { connect };
