ca3570de912db560358fd47d55c82d8ea34ebb37
Russel Mueller <bohjaangles@gmail.com>
2022-10-26 20:16:42 +0000

    refactored play.js to import its connect function from another file named client.js

--- play.js
+++ play.js
@@ -1,16 +1,3 @@
 const net = require('net');
+const connect = require('./client');
 
-const connnect = () => {
-  const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: 50541
-  })
-  conn.setEncoding('utf8');
-  conn.on('data', (data) => {
-    console.log('server says:', data);
-  })
-  return conn
-};
-
-console.log('connecting...');
-connnect();
